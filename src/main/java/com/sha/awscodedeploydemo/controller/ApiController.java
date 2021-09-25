8dcdd4a9cad11c3729e6f83e3c61b81eb3069f50
aron <aron@MacBook-Pro-de-Macbook.local>
2021-09-25 13:35:56 -0500

    testing cross origin

--- src/main/java/com/sha/awscodedeploydemo/controller/ApiController.java
+++ src/main/java/com/sha/awscodedeploydemo/controller/ApiController.java
@@ -1,14 +1,11 @@
 package com.sha.awscodedeploydemo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author sa
- * @date 6.02.2021
- * @time 14:05
- */
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class ApiController
 {
