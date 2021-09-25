03db569d7ca776f1b51a7c150f260159bbfb95dd
aron <aron@MacBook-Pro-de-Macbook.local>
2021-09-25 17:26:59 -0500

    changing api health noauthorized step

--- src/main/java/com/sha/awscodedeploydemo/controller/ApiController.java
+++ src/main/java/com/sha/awscodedeploydemo/controller/ApiController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@PreAuthorize("hasRole('USER')")
 public class ApiController
 {
     @GetMapping("/api/health")
