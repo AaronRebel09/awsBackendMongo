3e479416adc5af0ccdc5b0b9bd536c25f5cde6cc
aron <aron@MacBook-Pro-de-Macbook.local>
2021-09-25 16:39:22 -0500

    add pre authorized rol for api health

--- src/main/java/com/sha/awscodedeploydemo/controller/ApiController.java
+++ src/main/java/com/sha/awscodedeploydemo/controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.sha.awscodedeploydemo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @time 14:05
  */
 @RestController
+@PreAuthorize("hasRole('USER')")
 public class ApiController
 {
     @GetMapping("/api/health")
