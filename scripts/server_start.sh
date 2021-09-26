cdf7bd97afd14657590efae4b2a19f3bf0e035a3
aron <aron@MacBook-Pro-de-Macbook.local>
2021-09-25 23:02:55 -0500

    new horizon 02

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar -Dserver.port=8582 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+#sudo java -jar -Dserver.port=8582 \
+#    *.jar > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Dserver.port=8582 aws-code-deploy-demo-0.0.1-SNAPSHOT.jar  &
